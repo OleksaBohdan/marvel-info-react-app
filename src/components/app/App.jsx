10e26dfd5e013d5dfff55011ad5e91e299231a39
OleksaBohdan <bohdan.oleksa@gmail.com>
2023-03-16 13:32:50 -0600

    wip: started implement ComicsList

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -4,6 +4,8 @@ import RandomChar from '../randomChar/RandomChar';
 import CharList from '../charList/CharList';
 import CharInfo from '../charInfo/CharInfo';
 import ErrorBoundary from '../errorBoundary/ErrorBoundary';
+import AppBanner from '../appBanner/AppBanner';
+import ComicsList from '../comicsList/ComicsList';
 
 import decoration from '../../resources/img/vision.png';
 
@@ -18,7 +20,7 @@ const App = () => {
     <div className="app">
       <AppHeader />
       <main>
-        <ErrorBoundary>
+        {/* <ErrorBoundary>
           <RandomChar />
         </ErrorBoundary>
         <div className="char__content">
@@ -29,7 +31,14 @@ const App = () => {
             <CharInfo charId={selectedChar} />
           </ErrorBoundary>
         </div>
-        <img className="bg-decoration" src={decoration} alt="vision" />
+        <img className="bg-decoration" src={decoration} alt="vision" /> */}
+
+        <ErrorBoundary>
+          <AppBanner />
+        </ErrorBoundary>
+        <ErrorBoundary>
+          <ComicsList />
+        </ErrorBoundary>
       </main>
     </div>
   );
