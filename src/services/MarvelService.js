a4ef7f6b74b8ce8cc8df763d0664dee78577f4e7
OleksaBohdan <bohdan.oleksa@gmail.com>
2023-03-17 13:14:04 -0600

    feat: implemented ComicsList page

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -17,6 +17,28 @@ const useMarvelService = () => {
     return _transformCharacter(res.data.results[0]);
   };
 
+  const getAllComics = async (offset = 0) => {
+    const res = await request(`${_apiBase}/comics?limit=8&offset=${offset}&${_apiKey}`);
+    return res.data.results.map(_transformComics);
+  };
+
+  const getComics = async (id) => {
+    const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
+    return _transformComics(res.data.results[0]);
+  };
+
+  const _transformComics = (comics) => {
+    return {
+      id: comics.id,
+      title: comics.title,
+      description: comics.description || 'There is no description',
+      pageCount: comics.pageCount ? `${comics.pageCount} p.` : 'No information about the number of pages',
+      thumbnail: comics.thumbnail.path + '.' + comics.thumbnail.extension,
+      language: comics.textObjects[0]?.language || 'en-us',
+      price: comics.prices[0].price ? `${comics.prices[0].price}$` : 'not available',
+    };
+  };
+
   const _transformCharacter = (char) => {
     return {
       id: char.id,
@@ -31,7 +53,7 @@ const useMarvelService = () => {
     };
   };
 
-  return { loading, error, getAllCharacters, getCharacter, clearError };
+  return { loading, error, getAllCharacters, getCharacter, getAllComics, getComics, clearError };
 };
 
 export default useMarvelService;
