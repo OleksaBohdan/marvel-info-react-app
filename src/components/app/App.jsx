068c66bde049df0e74a6547e54fc28280cf9e449
OleksaBohdan <bohdan.oleksa@gmail.com>
2023-03-18 15:05:11 -0600

    feat: added routing

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,46 +1,20 @@
-import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AppHeader from '../appHeader/AppHeader';
-import RandomChar from '../randomChar/RandomChar';
-import CharList from '../charList/CharList';
-import CharInfo from '../charInfo/CharInfo';
-import ErrorBoundary from '../errorBoundary/ErrorBoundary';
-import AppBanner from '../appBanner/AppBanner';
-import ComicsList from '../comicsList/ComicsList';
-
-import decoration from '../../resources/img/vision.png';
+import { MainPage, ComicsPage } from '../pages';
 
 const App = () => {
-  const [selectedChar, setChar] = useState(null);
-
-  const onCharSelected = (id) => {
-    setChar(id);
-  };
-
   return (
-    <div className="app">
-      <AppHeader />
-      <main>
-        {/* <ErrorBoundary>
-          <RandomChar />
-        </ErrorBoundary>
-        <div className="char__content">
-          <ErrorBoundary>
-            <CharList onCharSelected={onCharSelected} />
-          </ErrorBoundary>
-          <ErrorBoundary>
-            <CharInfo charId={selectedChar} />
-          </ErrorBoundary>
-        </div>
-        <img className="bg-decoration" src={decoration} alt="vision" /> */}
-
-        <ErrorBoundary>
-          <AppBanner />
-        </ErrorBoundary>
-        <ErrorBoundary>
-          <ComicsList />
-        </ErrorBoundary>
-      </main>
-    </div>
+    <Router>
+      <div className="app">
+        <AppHeader />
+        <main>
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/comics" element={<ComicsPage />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 };
 
--- src/components/appHeader/AppHeader.jsx
+++ src/components/appHeader/AppHeader.jsx
@@ -1,22 +1,31 @@
+import { Link, NavLink } from 'react-router-dom';
 import './appHeader.scss';
 
 const AppHeader = () => {
-    return (
-        <header className="app__header">
-            <h1 className="app__title">
-                <a href="#">
-                    <span>Marvel</span> information portal
-                </a>
-            </h1>
-            <nav className="app__menu">
-                <ul>
-                    <li><a href="#">Characters</a></li>
-                    /
-                    <li><a href="#">Comics</a></li>
-                </ul>
-            </nav>
-        </header>
-    )
-}
+  return (
+    <header className="app__header">
+      <h1 className="app__title">
+        <Link to="/">
+          <span>Marvel</span> information portal
+        </Link>
+      </h1>
+      <nav className="app__menu">
+        <ul>
+          <li>
+            <NavLink end style={({ isActive }) => ({ color: isActive ? '#9f0013' : 'inherit' })} to="/">
+              Characters
+            </NavLink>
+          </li>
+          /
+          <li>
+            <NavLink end style={({ isActive }) => ({ color: isActive ? '#9f0013' : 'inherit' })} to="/comics">
+              Comics
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default AppHeader;
