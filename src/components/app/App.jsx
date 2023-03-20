da81aef1537f15124f774f72bc91903e2cd6c22c
OleksaBohdan <bohdan.oleksa@gmail.com>
2023-03-20 12:08:01 -0600

    feat: added 404 page

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AppHeader from '../appHeader/AppHeader';
-import { MainPage, ComicsPage } from '../pages';
+import { MainPage, ComicsPage, Page404 } from '../pages';
 
 const App = () => {
   return (
@@ -11,6 +11,8 @@ const App = () => {
           <Routes>
             <Route path="/" element={<MainPage />} />
             <Route path="/comics" element={<ComicsPage />} />
+
+            <Route path="*" element={<Page404 />} />
           </Routes>
         </main>
       </div>
